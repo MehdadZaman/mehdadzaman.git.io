490df86493c6c7a66143233e98643f6e049b9b44
Mehdad Zaman <zaman.mehdad.227@gmail.com>
2020-07-22 00:59:43 -0400

    website commit

---
+++ contact-form.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_POST)) {
+
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    if(empty($name) || mpty($mailFrom) || mpty($subject) || mpty($message)) {
+
+        echo "Please fill in all of the fields";
+
+    }
+    else {
+        
+        $mailTo = "zaman.mehdad.227@gmail.com";
+        $headers = "From: " .$mailFrom;
+
+        $txt = "Personal website email: \nYou recieved an email from " .$name. ".\n\n" .$message;
+
+        mail($mailTo, $subject, $txt, $headers);
+
+        header("Location: contact.html");
+
+        echo "<script type='text/javascript'>alert('Your message was sent');
+        window.history.log(-1);
+        </script>";
+    }
+}
