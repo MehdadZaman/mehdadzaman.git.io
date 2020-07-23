4ec2c80151f3431ded84924e91672afda3a4ce64
Mehdad Zaman <zaman.mehdad.227@gmail.com>
2020-07-23 19:40:11 -0400

    photo modal works

--- assets/js/main.js
+++ assets/js/main.js
@@ -323,18 +323,19 @@ function Delete() {
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-}
+
+Array.from(document.querySelectorAll(".ImgThumbnail")).forEach(item => {
+   item.addEventListener("click", event => {
+		modal.style.display = "block";
+      	modalImg.src = event.target.src;
+   });
+});
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+	modal.style.display = "none";
 }
